155112a43e7bf1ddb918e84d3a7673f6a37a9cf9
BlindeGardine1989 <sarahstefanowitz@yahoo.de>
2026-07-09 22:16:37 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,2 @@
-export const SUPABASE_URL = "https://jlvfchlbjaviqhfclvva.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Vpur2TPyGCFfIpxg3Prh0g__8s6uzY1";
-export const IS_CONFIGURED = SUPABASE_URL.startsWith("https://") && !SUPABASE_PUBLISHABLE_KEY.includes("HIER_DEINEN");
+export const SUPABASE_URL = "https://jlvfchlbjaviqhfclvva.supabase.co"
+export const SUPABASE_ANON_KEY = "sb_publishable_Vpur2TPyGCFfIpxg3Prh0g__8s6uzY1"
